6409fe1bd897b8555a9ee5d58c9a5c3880a33c9e
SARJEET SINGH SHEKHAWAT <devsarjeet14@gmail.com>
2025-10-06 23:09:15 +0530

    conftest.py

--- legistra/backend/tests/conftest.py
+++ legistra/backend/tests/conftest.py
@@ -5,7 +5,7 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['MONGODB_URI'] = 'mongodb+srv://Sarjeet:1404@1.1locgnd.mongodb.net/?retryWrites=true&w=majority&appName=1'
+    app.config['MONGODB_URI'] = 'mongodb+srv://Username: Password@1.1locgnd.mongodb.net/?retryWrites=true&w=majority&appName=1'
     app.config['MONGO_DB'] = 'test_legal_docs'
     app.config['UPLOAD_FOLDER'] = tempfile.mkdtemp()
     with app.test_client() as client:
